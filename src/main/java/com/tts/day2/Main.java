0ff7ef57044ec96f8c99abd912a285c3ab8f9fee
Franklyn Reyes <franklynreyes777@yahoo.com>
2021-04-27 10:19:20 -0400

    Initial commit

---
+++ src/main/java/com/tts/day2/Main.java
@@ -0,0 +1,39 @@
+package com.tts.day2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Tree childTree = new Tree("Oak", "21", new Tree());
+        Tree myTree = new Tree("Birch", "30", childTree);
+
+        System.out.println(myTree.getTree());
+
+        NestingDoll fatherDoll = new NestingDoll("Father");
+        NestingDoll motherDoll = new NestingDoll("Mother");
+        NestingDoll childDoll = new NestingDoll("Child");
+
+        fatherDoll.setInnerDoll(childDoll);
+        motherDoll.setInnerDoll(fatherDoll);
+
+        System.out.println(motherDoll);
+
+        BinaryTree binaryTree = new BinaryTree();
+
+        int[] nodes = {3 ,2, 4, 10, 1, 7, 11};
+
+        for (int n: nodes) {
+            binaryTree.add(n);
+        }
+
+        System.out.println(binaryTree);
+
+        BinaryHeap binaryHeap = new BinaryHeap();
+        int[] nodesHeap = {1, 12, 3, 24, 7};
+
+        for (int n: nodesHeap) {
+            binaryHeap.heap.add(n);
+        }
+
+        System.out.println(binaryHeap);
+    }
+}
